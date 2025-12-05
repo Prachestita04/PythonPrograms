61d6d13dda232b265ad5d7cc5bd56304b61b66c1
Prachestita04 <prachestitamahapatra@gmail.com>
2025-12-05 11:54:05 +0530

    operator programs

--- PythonProgramPractice/Program_5_Operators.py
+++ PythonProgramPractice/Program_5_Operators.py
@@ -1,5 +1,22 @@
 '''
 python operators = arithmatic operators,relational operators,assignment operators,logical operators,other operators
 Arithmatic operators = addition(+),subtraction(-),multiplication(*),division(/),modulos(%),floor division(//),exponential(**)
-relational operators
+relational operators = < , > , <= , >= , ==, !=
+assignment operators = =, +=, -=, *= , /= , %= , //= etc.
+logical operators = and, or, not
+other operators =  identity operator, bitwise operator, membership operator
 '''
+a, b = 10, 5
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+print(a ** b)
+
+print(True and True)
+print(True or False)
+print(not True)
+print(not False)
+print(False or False)

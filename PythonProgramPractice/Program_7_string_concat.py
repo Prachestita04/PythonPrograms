594919a02a5c289d41e564bd27807bdb1c160504
Prachestita04 <prachestitamahapatra@gmail.com>
2025-12-05 12:26:59 +0530

    string program

---
+++ PythonProgramPractice/Program_7_string_concat.py
@@ -0,0 +1,20 @@
+print("Prachi" + "Mahapatra")
+print(5 + 4)  # operator
+# print("Mamuni"+9) gives error
+print("Mamuni" + str(9))
+
+# store and print multiline string
+
+s = """
+    Hey Mamuni....
+       good morning
+    How are you
+        what are you doing now??????
+"""
+print(s)
+
+# store a lengthy text in a variable and print
+text = "I am Prachestita Mahapatra.\n"\
+ "My favourite programming language is python."\
+  "I am working on a python project. "
+print(text)
